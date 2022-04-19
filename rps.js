f36546deeefe189554fb69a978d8f5619a2f5d99
Nicholas Jubelt <nickjubelt@gmail.com>
2022-04-19 13:51:12 -0400

    Add playRound function

--- rps.js
+++ rps.js
@@ -1,12 +1,26 @@
 function computerPlay() {
      play = Math.floor(Math.random()*3);
      if (play == 0) {
-          return 'Rock'
+          return 'rock'
      }
      else if (play == 1) {
-          return 'Paper'
+          return 'paper'
      }
      else {
-          return 'Scissors'
+          return 'scissors'
+     }
+}
+
+function playRound(playerSelection, computerSelection) {
+     playerSelection = playerSelection.toLowerCase();
+     computerSelection = computerPlay();
+     if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
+          console.log('Congrats, ' + playerSelection + ' beats ' + computerSelection + '!');
+     }
+     else if (playerSelection == computerSelection) {
+          console.log('Tie! You both picked ' + playerSelection + '.');
+     }
+     else {
+          console.log('Sorry, ' + computerSelection + ' beats ' + playerSelection + '.');
      }
 }
